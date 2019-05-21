7e2fe89eb5d443a5bd22db64d9f3411faba07bc7
Mwangi <8320816+kidynamit@users.noreply.github.com>
2019-05-21 17:28:20 +0200

    Initial Commit

---
+++ Common/Core/module.cmake
@@ -0,0 +1,19 @@
+vtk_module(vtkCommonCore
+  GROUPS
+    StandAlone
+  TEST_DEPENDS
+    vtkTestingCore
+    vtkCommonDataModel
+    vtkCommonSystem
+    vtkCommonTransforms
+    vtkFiltersProgrammable
+    vtkFiltersVerdict
+    vtksys
+  KIT
+    vtkCommon
+  DEPENDS
+    vtkkwiml
+  PRIVATE_DEPENDS
+    vtksys
+    vtkutf8
+  )
---
+++ Rendering/OpenVR/module.cmake
@@ -0,0 +1,31 @@
+if (VTK_OPENVR_OBJECT_FACTORY)
+  set (_impl IMPLEMENTS vtkRenderingOpenGL2)
+endif()
+
+vtk_module(vtkRenderingOpenVR
+  BACKEND
+    OpenGL2
+  DEPENDS
+    vtksys
+    vtkCommonCore
+    vtkCommonSystem
+    vtkRenderingOpenGL2
+    vtkRenderingVolumeOpenGL2
+    vtkInteractionStyle
+    vtkInteractionWidgets
+    vtkIOCore
+    vtkIOImage
+    vtkIOXMLParser
+  ${_impl}
+  PRIVATE_DEPENDS
+    vtkglew
+    vtkImagingSources
+    vtkFiltersSources
+  TEST_DEPENDS
+    vtkTestingCore
+    vtkTestingRendering
+    vtkInteractionWidgets
+    vtkRenderingVolume
+    vtkIOPLY
+    vtkIOXML
+)
